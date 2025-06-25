96349f0e3505e03787c86ced2a76b2757b06dc00
mohamed sallam <mohamedsallam35@gmail.com>
2025-06-25 11:09:00 +0300

    Update SQLQuery2vacc.sql

--- SQLQuery2vacc.sql
+++ SQLQuery2vacc.sql
@@ -2,15 +2,21 @@
 --SELECT * FROM PORTOFOLIO..CovidVaccinations
 --order by 3,4
 
+--select by location 
+
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from PORTOFOLIO..CovidDeaths
 where "location" = 'Egypt' 
 ORDER by 1,2
 
+ --handiling the null
+
 select location,population, date, total_cases, round((total_cases/population)*100,5) as casepercentage
 from PORTOFOLIO..CovidDeaths
 where "location" = 'Egypt' and continent is not null
 ORDER by total_cases desc
+
+ --the persentage of total cases over population 
 
 select location,population, max(total_cases) as highstinfection, max((total_cases/population))*100 as casepercentage
 from PORTOFOLIO..CovidDeaths 
@@ -18,6 +24,7 @@ where continent is not null
 group by location, population 
 ORDER by casepercentage desc
 
+ --total deaths 
 
 select location, max(cast(total_deaths as int)) as TotoalDeathCount
 from PORTOFOLIO..CovidDeaths 
@@ -25,7 +32,7 @@ where continent is not null
 group by location, population 
 ORDER by TotoalDeathCount desc
 
-
+--total death by continent
 
 select continent, max(cast(total_deaths as int)) as TotoalDeathCount
 from PORTOFOLIO..CovidDeaths 
@@ -33,12 +40,15 @@ where continent is not null
 group by continent 
 ORDER by TotoalDeathCount desc
 
-
+ 
+--death percentage
+ 
 select sum(new_cases) as totalCases , sum(cast(new_deaths as int)) as TotalDeaths , sum(cast(new_deaths as int))/sum(new_cases)*100 as deathpercentage
 from PORTOFOLIO..CovidDeaths 
 where continent is not null
 ORDER by 1,2
 
+ --join
 
 select *
  from PORTOFOLIO..CovidDeaths as d
